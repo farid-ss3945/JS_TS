69715334790e02c98be988c46992f1ce3e134fe6
Farid Abbasli <abbaslifarid2006@gmail.com>
2026-03-02 20:29:12 +0400

    Delete source/repos/WebApplication5 directory

--- source/repos/WebApplication5/WebApplication5/DTOs/InvoiceRowDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication5.DTOs
-{
-    public class InvoiceRowDto
-    {
-        public string Service { get; set; } = null!;
-        public decimal Quantity { get; set; }
-        public decimal Rate { get; set; }
-    }
-}
